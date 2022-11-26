d1ef42eed8651241e300a0a7f46db25f41ffd209
williamquesnot <william.quesnot@epsi.fr>
2022-11-26 10:35:00 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  
+  const [menu, setMenu] = useState(false);
+
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,61 +19,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section style={{backgroundColor : 'rgb(10, 25, 47)', opacity : 0.8, height : '80px', width : '100%', position : 'fixed'}}>
+        <div style={{width : '100%', height : '100%', display : 'flex', justifyContent : 'flex-end'}}>
+          <button onClick={()=> setMenu(true)} style={{display : 'flex', alignItems : 'center', justifyContent : 'center', width : '80px', backgroundColor : 'unset', border : 'none'}}><p style={{fontSize : '3.5em', color : 'white'}}>☰</p></button>
         </div>
-      </main>
+        {menu &&(
+          <div style={{position :'absolute', top : '0px', width : '100%'}}>
+            <div style={{height : '320px', width : '100%', backgroundColor :'rgb(10,10,10)', display : 'flex', flexDirection : 'column', justifyContent : 'center', alignItems : 'center'}}>
+              <Link href="ok">À PROPOS DE MOI</Link>
+              <br/>
+              <Link href="ok">COMPÉTENCES</Link>
+              <br/>
+              <Link href="ok">EXPÉRIENCES</Link>
+              <br/>
+              <Link href="ok">PROJETS</Link>
+              <br/>
+              <Link href="ok">ME CONTACTER</Link>
+              <br/>
+              <button onClick={()=> setMenu(false)} style={{backgroundColor : 'unset', border : 'none', margin : 'unset', padding : 'unset', height : '50px', display : 'flex', alignItems :'center', justifyContent : 'center'}}><p style={{fontSize : '4em', color : 'white'}}>&times;</p></button>
+            </div>  
+          </div>
+        )}
+        
+      </section>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
+
+
